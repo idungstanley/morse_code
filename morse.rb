3c800b534cfbff92c522e00f9f6889bb7c7a30b1
sunday stanley Idung <sundaystanley56@gmail.com>
2022-07-21 19:07:28 +0100

    Add function to the code

--- morse.rb
+++ morse.rb
@@ -1,4 +1,4 @@
-IMC = {
+DICTIONARY = {
   '.-' => 'A',
   '-...' => 'B',
   '-.-.' => 'C',
@@ -26,3 +26,24 @@ IMC = {
   '-.--' => 'Y',
   '--..' => 'Z'
 }.freeze
+
+
+def decode_letter(letter)
+ DICTIONARY[letter]
+end
+
+def decode_word(word)
+ result = ''
+ letters = word.split
+ letters.each { |letter| result += decode_letter(letter) }
+ result
+end
+
+def decode(message)
+ result = ''
+ words = message.split('   ')
+ words.each { |word| result += "#{decode_word(word)} " }
+ result.strip!
+end
+
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
